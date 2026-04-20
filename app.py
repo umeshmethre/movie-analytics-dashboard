ae1dc6ac3083076365ac40406d4a925281ce4be6
Umesh methre <umeshmethre7@gmail.com>
2026-04-21 01:03:41 +0530

    Add seaborn for improved visualization styling

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
 
 # ML
 from sklearn.feature_extraction.text import CountVectorizer
